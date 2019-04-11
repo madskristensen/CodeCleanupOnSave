a78a9823b8108a19451aa40736e46b128a846874
Mads Kristensen <post@madskristensen.net>
2019-04-11 13:10:16 -0700

    Moved options page

--- src/CodeCleanupOnSavePackage.cs
+++ src/CodeCleanupOnSavePackage.cs
@@ -6,7 +6,7 @@ namespace CodeCleanupOnSave
 {
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
-    [ProvideOptionPage(typeof(DialogPageProvider.General), "Environment\\Documents", "Code Cleanup", 0, 0, true, ProvidesLocalizedCategoryName = false)]
+    [ProvideOptionPage(typeof(DialogPageProvider.General), "Environment", "Code Cleanup on Save", 0, 0, true, new[] { "Code Cleanup on Save" }, ProvidesLocalizedCategoryName = false)]
     [Guid("aee4d337-3c9a-4f9b-86ae-6dd7bd751bd5")]
     public sealed class CodeCleanupOnSavePackage : AsyncPackage
     {
