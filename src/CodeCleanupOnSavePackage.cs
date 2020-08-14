6622604ea78009b7db040684f49c0acab12b2f5b
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-08-14 14:14:21 -0700

    Added support for roaming settings Fixes #8

--- src/CodeCleanupOnSavePackage.cs
+++ src/CodeCleanupOnSavePackage.cs
@@ -7,9 +7,10 @@ namespace CodeCleanupOnSave
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
     [ProvideOptionPage(typeof(DialogPageProvider.General), "Environment", "Code Cleanup on Save", 0, 0, true, new[] { "Code Cleanup on Save" }, ProvidesLocalizedCategoryName = false)]
+    [ProvideProfile(typeof(DialogPageProvider.General), "Environment", Vsix.Name, 0, 0, true)]
     [Guid("aee4d337-3c9a-4f9b-86ae-6dd7bd751bd5")]
     public sealed class CodeCleanupOnSavePackage : AsyncPackage
     {
-        
+
     }
 }
